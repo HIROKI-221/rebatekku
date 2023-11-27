c7c7670054a6f432104c3d167214eb67be9135e1
HIROKI-221 <hiroki1123221@gmail.com>
2023-11-27 20:46:05 +0900

    dev02やり直し１

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () { return view('welcome'); });
+Route::get('/', function() {
+    return view('posts.index');
+});
 Route::get('/posts', [PostController::class, 'index']);
